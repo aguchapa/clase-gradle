0ab6f8820e80ca887327181ffb1fe965882d2251
Roberto Agustin Marcialis <41978046+aguchapa@users.noreply.github.com>
2024-09-04 18:46:15 -0300

    Se agrega  dependencia springframework (#2)
    * Se agrega springframework
    
    * Se agrega springframework
    
    ---------
    
    Co-authored-by: Marcela <agustinmarcialis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.kotlin.jvm)
-
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 repositories {
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-  //  implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
 
@@ -36,10 +36,6 @@ java {
     }
 }
 
-application {
-    // Define the main class for the application.
-    mainClass = "org.example.AppKt"
-}
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
